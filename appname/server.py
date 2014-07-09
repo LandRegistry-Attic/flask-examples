49273b9eb13acd15633bcf78661cd913ac3f25b4
Andy Porter <andyporter@Andys-MacBook-Pro.local>
2014-07-09 14:47:06 +0100

    route app to gov template

--- appname/server.py
+++ appname/server.py
@@ -1,6 +1,12 @@
+from flask import render_template
 from appname import app, db
 from  models import Foo
 
+# govuk_template asset path
+@app.context_processor
+def asset_path_context_processor():
+    return {'asset_path': '/static/govuk_template/'}
+
 @app.route('/')
 def index():
-    return "this is the index"
+    return render_template("landregistry_base.html")
