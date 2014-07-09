7f1a3a85a0e42ad573b39a342e4480cd27fa9255
Demotive <mat@demotive.com>
2014-07-09 15:00:44 +0100

    Resolving conflict with master

--- appname/server.py
+++ appname/server.py
@@ -10,3 +10,13 @@ def asset_path_context_processor():
 @app.route('/')
 def index():
     return render_template("landregistry_base.html")
+
+#  Some useful headers to set to beef up the robustness of the app
+# https://www.owasp.org/index.php/List_of_useful_HTTP_headers
+@app.after_request
+def after_request(response):
+    response.headers.add('Content-Security-Policy', "default-src 'self'")
+    response.headers.add('X-Frame-Options', 'deny')
+    response.headers.add('X-Content-Type-Options', 'nosniff')
+    response.headers.add('X-XSS-Protection', '1; mode=block')
+    return response
